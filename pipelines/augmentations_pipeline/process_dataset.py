11f2e25b1938b25c2c495b2e12cd2a2dddad1ef0
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-03-13 11:29:52 +0100

    fix: apply cv engine changes

---
+++ pipelines/augmentations_pipeline/process_dataset.py
@@ -0,0 +1,6 @@
+from picsellia_cv_engine.decorators.step_decorator import step
+
+
+@step
+def process_dataset():
+    pass
--- pipelines/augmentations_pipeline/processing_pipeline.py
+++ pipelines/augmentations_pipeline/processing_pipeline.py
@@ -1,14 +1,11 @@
-from examples.augmentation.utils.common import process_dataset
 from picsellia_cv_engine.decorators.pipeline_decorator import pipeline
 from picsellia_cv_engine.models.utils.picsellia_context import (
     create_picsellia_processing_context,
 )
-from picsellia_cv_engine.steps.data_extraction.processing_data_extractor import (
-    get_processing_dataset_collection,
-)
-from picsellia_cv_engine.steps.data_upload.dataset_context_uploader import (
-    upload_dataset_context,
-)
+from picsellia_cv_engine.steps.dataset.loader import load_coco_datasets
+from picsellia_cv_engine.steps.dataset.uploader import upload_full_dataset
+
+from pipelines.augmentations_pipeline.process_dataset import process_dataset
 
 processing_context = create_picsellia_processing_context(
     processing_parameters={
@@ -25,11 +22,11 @@ processing_context = create_picsellia_processing_context(
     remove_logs_on_completion=False,
 )
 def augmentations_pipeline():
-    dataset_collection = get_processing_dataset_collection()
+    dataset_collection = load_coco_datasets()
     dataset_collection["output"] = process_dataset(
         dataset_collection["input"], dataset_collection["output"]
     )
-    upload_dataset_context(dataset_collection["output"], use_id=False)
+    upload_full_dataset(dataset_collection["output"], use_id=False)
     return dataset_collection
 
 
