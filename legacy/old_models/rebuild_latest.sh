308c1503eb1e80d91918792c682c37c6c199c572
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-04-29 10:01:24 +0000

    fix: directory names

--- legacy/old_models/rebuild_latest.sh
+++ legacy/old_models/rebuild_latest.sh
@@ -1,11 +1,20 @@
-docker build . -f yolov8-classification/Dockerfile -t picsellia/training-yolov8-classification:latest
-docker push picsellia/training-yolov8-classification:latest
+docker build . -f tf2/Dockerfile -t picsellia/training-tf2:latest
+docker push picsellia/training-tf2:latest
 
-docker build . -f yolov8-detection/Dockerfile -t picsellia/training-yolov8-detection:latest
-docker push picsellia/training-yolov8-detection:latest
+docker build . -f keras_classification/Dockerfile -t picsellia/training-keras-classification:latest
+docker push picsellia/training-keras-classification:latest
 
-docker build . -f yolov8-segmentation/Dockerfile -t picsellia/training-yolov8-segmentation:latest
-docker push picsellia/training-yolov8-segmentation:latest
+docker build . -f yolov5_detection/Dockerfile -t picsellia/training-yolov5-detection:latest
+docker push picsellia/training-yolov5-detection:latest
 
-docker build . -f unet-instance-segmentation/Dockerfile -t picsellia/training-unet-segmentation:latest
+docker build . -f yolov5_segmentation/Dockerfile -t picsellia/training-yolov5-segmentation:latest
+docker push picsellia/training-yolov5-segmentation:latest
+
+docker build . -f unet_instance-segmentation/Dockerfile -t picsellia/training-unet-segmentation:latest
 docker push picsellia/training-unet-segmentation:latest
+
+docker build . -f ViT_classification/Dockerfile -t picsellia/training-vit-classification:latest
+docker push picsellia/training-vit-classification:latest
+
+docker build . -f yolox_detection/Dockerfile -t picsellia/training-yolox-detection:latest
+docker push picsellia/training-yolox-detection:latest
