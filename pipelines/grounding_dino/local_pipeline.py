f4e69793d6a2af17780957adaa84143b89cf31c1
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-07-01 15:16:42 +0200

    feat: use cv engine steps

--- pipelines/grounding_dino/local_pipeline.py
+++ pipelines/grounding_dino/local_pipeline.py
@@ -6,22 +6,27 @@ from picsellia_cv_engine.core.services.utils.local_context import (
 from picsellia_cv_engine.decorators.pipeline_decorator import pipeline
 from picsellia_cv_engine.steps.base.dataset.loader import load_coco_datasets
 from picsellia_cv_engine.steps.base.dataset.uploader import upload_dataset_annotations
-from picsellia_cv_engine.steps.base.model.builder import build_model
+from picsellia_cv_engine.steps.base.model.prediction_converter import (
+    convert_predictions_to_coco,
+)
+from picsellia_cv_engine.steps.grounding_dino.model.loader import (
+    load_grounding_dino_model,
+)
+from picsellia_cv_engine.steps.grounding_dino.model.predictor import (
+    run_grounding_dino_inference,
+)
+from utils.parameters import ProcessingParameters
 
-from pipelines.grounding_dino.steps import process
-from pipelines.grounding_dino.utils.parameters import GroundingDinoProcessingParameters
-
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description="Run the local processing pipeline")
+parser = argparse.ArgumentParser(description="Run the local preannotation pipeline")
 parser.add_argument("--api_token", required=True, type=str, help="Picsellia API token")
 parser.add_argument(
-    "--organization_name", required=True, type=str, help="Picsellia Organization ID"
+    "--organization_name", required=True, type=str, help="Picsellia organization name"
 )
 parser.add_argument(
     "--job_type",
     required=True,
     type=str,
-    choices=["DATASET_VERSION_CREATION", "TRAINING"],
+    choices=["DATASET_VERSION_CREATION", "PRE_ANNOTATION", "TRAINING"],
     help="Job type",
 )
 parser.add_argument(
@@ -31,26 +36,20 @@ parser.add_argument(
     help="Input dataset version ID",
 )
 parser.add_argument(
-    "--output_dataset_version_name",
-    required=False,
-    type=str,
-    help="Output dataset version name",
-    default=None,
+    "--model_version_id", required=True, type=str, help="Model version ID"
 )
 parser.add_argument(
     "--working_dir", required=False, type=str, help="Working directory", default=None
 )
 args = parser.parse_args()
 
-# Create local processing context
 context = create_local_processing_context(
-    processing_parameters_cls=GroundingDinoProcessingParameters,
+    processing_parameters_cls=ProcessingParameters,
     api_token=args.api_token,
     organization_name=args.organization_name,
     job_type=args.job_type,
     input_dataset_version_id=args.input_dataset_version_id,
-    output_dataset_version_name=args.output_dataset_version_name,
-    model_version_id="0196cf7c-11fe-797d-9c55-d1b4a59a594c",
+    model_version_id=args.model_version_id,
     working_dir=args.working_dir,
 )
 
@@ -62,11 +61,16 @@ context = create_local_processing_context(
 )
 def grounding_dino_pipeline():
     picsellia_dataset = load_coco_datasets()
-    picsellia_model = build_model(
+    grounding_dino_model = load_grounding_dino_model(
         pretrained_weights_name="pretrained-weights", config_name="config"
     )
-    picsellia_dataset = process(
-        picsellia_model=picsellia_model, picsellia_dataset=picsellia_dataset["input"]
+    predictions = run_grounding_dino_inference(
+        model=grounding_dino_model,
+        dataset=picsellia_dataset,
+    )
+    picsellia_dataset = convert_predictions_to_coco(
+        predictions=predictions,
+        picsellia_dataset=picsellia_dataset,
     )
     upload_dataset_annotations(dataset=picsellia_dataset, use_id=True)
 
--- pipelines/grounding_dino/picsellia_pipeline.py
+++ pipelines/grounding_dino/picsellia_pipeline.py
@@ -4,13 +4,19 @@ from picsellia_cv_engine.core.services.utils.picsellia_context import (
 from picsellia_cv_engine.decorators.pipeline_decorator import pipeline
 from picsellia_cv_engine.steps.base.dataset.loader import load_coco_datasets
 from picsellia_cv_engine.steps.base.dataset.uploader import upload_dataset_annotations
-from picsellia_cv_engine.steps.base.model.builder import build_model
-
-from pipelines.grounding_dino.steps import process
-from pipelines.grounding_dino.utils.parameters import GroundingDinoProcessingParameters
+from picsellia_cv_engine.steps.base.model.prediction_converter import (
+    convert_predictions_to_coco,
+)
+from picsellia_cv_engine.steps.grounding_dino.model.loader import (
+    load_grounding_dino_model,
+)
+from picsellia_cv_engine.steps.grounding_dino.model.predictor import (
+    run_grounding_dino_inference,
+)
+from utils.parameters import ProcessingParameters
 
 processing_context = create_picsellia_processing_context(
-    processing_parameters_cls=GroundingDinoProcessingParameters,
+    processing_parameters_cls=ProcessingParameters,
 )
 
 
@@ -21,11 +27,16 @@ processing_context = create_picsellia_processing_context(
 )
 def grounding_dino_pipeline():
     picsellia_dataset = load_coco_datasets()
-    picsellia_model = build_model(
+    grounding_dino_model = load_grounding_dino_model(
         pretrained_weights_name="pretrained-weights", config_name="config"
     )
-    picsellia_dataset = process(
-        picsellia_model=picsellia_model, picsellia_dataset=picsellia_dataset
+    predictions = run_grounding_dino_inference(
+        model=grounding_dino_model,
+        dataset=picsellia_dataset,
+    )
+    picsellia_dataset = convert_predictions_to_coco(
+        predictions=predictions,
+        picsellia_dataset=picsellia_dataset,
     )
     upload_dataset_annotations(dataset=picsellia_dataset, use_id=True)
 
