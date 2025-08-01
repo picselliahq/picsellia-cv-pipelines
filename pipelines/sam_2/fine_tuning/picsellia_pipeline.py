dde489a0a73950f8afad6a268e48304fd89ec82d
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-08-01 09:29:30 +0000

    fix: use cv engine steps

--- pipelines/sam_2/fine_tuning/picsellia_pipeline.py
+++ pipelines/sam_2/fine_tuning/picsellia_pipeline.py
@@ -1,3 +1,5 @@
+import os
+
 from picsellia_cv_engine import pipeline
 from picsellia_cv_engine.core.parameters import (
     AugmentationParameters,
@@ -10,7 +12,8 @@ from picsellia_cv_engine.steps.base.dataset.loader import (
     load_coco_datasets,
 )
 from picsellia_cv_engine.steps.base.model.builder import build_model
-from steps import evaluate, train
+from picsellia_cv_engine.steps.sam2.model.evaluator import evaluate
+from picsellia_cv_engine.steps.sam2.model.trainer import train
 from utils.parameters import TrainingHyperParameters
 
 context = create_picsellia_training_context(
@@ -21,12 +24,16 @@ context = create_picsellia_training_context(
 
 
 @pipeline(context=context, log_folder_path="logs/", remove_logs_on_completion=False)
-def training_pipeline():
+def fine_tuning_pipeline():
     picsellia_datasets = load_coco_datasets()
     picsellia_model = build_model(pretrained_weights_name="pretrained-weights")
-    train(picsellia_model=picsellia_model, picsellia_datasets=picsellia_datasets)
-    evaluate(picsellia_model=picsellia_model, dataset=picsellia_datasets["test"])
+    train(
+        model=picsellia_model,
+        dataset_collection=picsellia_datasets,
+        sam2_repo_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "sam2"),
+    )
+    evaluate(model=picsellia_model, dataset=picsellia_datasets["test"])
 
 
 if __name__ == "__main__":
-    training_pipeline()
+    fine_tuning_pipeline()
