040bcadf1b82a51beeb52a0ce9de1c6a4e0dde0d
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-08-28 18:46:38 +0200

    fix: update

--- pipelines/grounding_dino/pipeline.py
+++ pipelines/grounding_dino/pipeline.py
@@ -1,5 +1,8 @@
-from picsellia_cv_engine.core.services.utils.picsellia_context import (
-    create_picsellia_processing_context,
+import argparse
+
+from picsellia.types.enums import ProcessingType
+from picsellia_cv_engine.core.services.context.unified_context import (
+    create_processing_context_from_config,
 )
 from picsellia_cv_engine.decorators.pipeline_decorator import pipeline
 from picsellia_cv_engine.steps.base.dataset.loader import load_coco_datasets
@@ -15,13 +18,21 @@ from picsellia_cv_engine.steps.grounding_dino.model.predictor import (
 )
 from utils.parameters import ProcessingParameters
 
-processing_context = create_picsellia_processing_context(
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", choices=["local", "picsellia"], default="picsellia")
+parser.add_argument("--config-file", type=str, required=False)
+args = parser.parse_args()
+
+context = create_processing_context_from_config(
+    processing_type=ProcessingType.PRE_ANNOTATION,
     processing_parameters_cls=ProcessingParameters,
+    mode=args.mode,
+    config_file_path=args.config_file,
 )
 
 
 @pipeline(
-    context=processing_context,
+    context=context,
     log_folder_path="logs/",
     remove_logs_on_completion=False,
 )
