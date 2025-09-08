8a5c25edddcb127137292daa3011933b46d9579f
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-09-08 15:21:10 +0200

    fix: update

--- pipelines/yolov8/training/pipeline.py
+++ pipelines/yolov8/training/pipeline.py
@@ -1,11 +1,12 @@
+import argparse
+
 from picsellia_cv_engine import pipeline
-from picsellia_cv_engine.core.contexts import PicselliaTrainingContext
 from picsellia_cv_engine.core.parameters import ExportParameters
+from picsellia_cv_engine.core.services.context.unified_context import (
+    create_training_context_from_config,
+)
 from picsellia_cv_engine.frameworks.ultralytics.parameters.augmentation_parameters import (
     UltralyticsAugmentationParameters,
-)
-from picsellia_cv_engine.frameworks.ultralytics.parameters.hyper_parameters import (
-    UltralyticsHyperParameters,
 )
 from picsellia_cv_engine.steps.ultralytics.dataset.preparator import (
     prepare_ultralytics_dataset,
@@ -19,23 +20,27 @@ from picsellia_cv_engine.steps.ultralytics.model.exporter import (
 from picsellia_cv_engine.steps.ultralytics.model.loader import (
     load_ultralytics_model,
 )
-from picsellia_cv_engine.steps.ultralytics.model.trainer import (
-    train_ultralytics_model,
+from steps import simple_train_ultralytics_model
+from utils.parameters import (
+    UltralyticsHyperParameters,
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", choices=["local", "picsellia"], default="picsellia")
+parser.add_argument("--config-file", type=str, required=False)
+args = parser.parse_args()
+
+context = create_training_context_from_config(
+    hyperparameters_cls=UltralyticsHyperParameters,
+    augmentation_parameters_cls=UltralyticsAugmentationParameters,
+    export_parameters_cls=ExportParameters,
+    mode=args.mode,
+    config_file_path=args.config_file,
 )
 
 
-def get_context() -> PicselliaTrainingContext[
-    UltralyticsHyperParameters, UltralyticsAugmentationParameters, ExportParameters
-]:
-    return PicselliaTrainingContext(
-        hyperparameters_cls=UltralyticsHyperParameters,
-        augmentation_parameters_cls=UltralyticsAugmentationParameters,
-        export_parameters_cls=ExportParameters,
-    )
-
-
 @pipeline(
-    context=get_context(),
+    context=context,
     log_folder_path="logs/",
     remove_logs_on_completion=False,
 )
@@ -44,7 +49,7 @@ def yolov8_training_pipeline():
 
     model = load_ultralytics_model(pretrained_weights_name="pretrained-weights")
 
-    train_ultralytics_model(model=model, dataset_collection=dataset_collection)
+    simple_train_ultralytics_model(model=model, dataset_collection=dataset_collection)
 
     export_ultralytics_model(model=model)
 
@@ -52,4 +57,11 @@ def yolov8_training_pipeline():
 
 
 if __name__ == "__main__":
+    import gc
+
+    import torch
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
     yolov8_training_pipeline()
