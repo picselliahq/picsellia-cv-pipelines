29ec339aaf1f9e87c20fd847af4aa0c7ba632a69
SoniaGrh <sonia.garrouch@hotmail.fr>
2026-01-07 15:43:19 +0100

    fix: batch size

--- pipelines/tf2/utils/parameters.py
+++ pipelines/tf2/utils/parameters.py
@@ -6,8 +6,18 @@ class TrainingHyperParameters(HyperParameters):
     def __init__(self, log_data: LogDataType):
         super().__init__(log_data=log_data)
         self.steps = self.extract_parameter(["steps"], expected_type=int, default=3)
-        self.batch_size = self.extract_parameter(["batch_size"], expected_type=int, default=8)
-        self.image_size = self.extract_parameter(["image_size"], expected_type=int, default=640)
-        self.learning_rate = self.extract_parameter(["learning_rate"], expected_type=float, default=0.0005)
-        self.annotation_type = self.extract_parameter(["annotation_type"], expected_type=str, default="polygon") # ou rectangle
-        self.checkpoint_every_n = self.extract_parameter(["checkpoint_every_n"], expected_type=int, default=10)
+        self.batch_size = self.extract_parameter(
+            ["batch_size"], expected_type=int, default=1
+        )
+        self.image_size = self.extract_parameter(
+            ["image_size"], expected_type=int, default=640
+        )
+        self.learning_rate = self.extract_parameter(
+            ["learning_rate"], expected_type=float, default=0.0005
+        )
+        self.annotation_type = self.extract_parameter(
+            ["annotation_type"], expected_type=str, default="polygon"
+        )  # ou rectangle
+        self.checkpoint_every_n = self.extract_parameter(
+            ["checkpoint_every_n"], expected_type=int, default=10
+        )
