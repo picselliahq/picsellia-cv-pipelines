3aea8216cec1beb0b07f6cc44418708032b66925
Valhugnatt <valentin.philipp@picsellia.com>
2026-03-31 16:24:00 +0200

    default parameter value update to best-model and add description

--- pipelines/yolov8/pre_annotation/utils/parameters.py
+++ pipelines/yolov8/pre_annotation/utils/parameters.py
@@ -6,7 +6,7 @@ class ProcessingYOLOV8PreannotationParameters(Parameters):
         super().__init__(log_data)
 
         self.model_file_name = self.extract_parameter(
-            keys=["model_file_name"], expected_type=str, default="pretrained-weights"
+            keys=["model_file_name"], expected_type=str, default= "best-model"
         )
         self.confidence_threshold = self.extract_parameter(
             keys=["confidence_threshold"], expected_type=float, default=0.1
