0417299dab5c27fefac07930d00b6f8cf23f877c
Valhugnatt <valentin.philipp@picsellia.com>
2026-05-29 17:02:04 +0200

    first update

--- pipelines/split_train_test_val/utils/parameters.py
+++ pipelines/split_train_test_val/utils/parameters.py
@@ -5,9 +5,6 @@ from picsellia_cv_engine.core.parameters import Parameters
 class ProcessingParameters(Parameters):
     def __init__(self, log_data: LogDataType):
         super().__init__(log_data=log_data)
-        self.ratio_train = self.extract_parameter(["ratio_train"], expected_type=float, default=0.7)
-        self.ratio_test = self.extract_parameter(["ratio_test"], expected_type=float, default=0.15)
-        self.ratio_val = self.extract_parameter(["ratio_val"], expected_type=float, default=0.15)
         self.embed_asset_without_annotation = self.extract_parameter(["embed_asset_without_annotation"], expected_type=bool, default=True)
         self.add_asset_tags = self.extract_parameter(["add_asset_tags"], expected_type=bool, default=True)
 
