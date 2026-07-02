1f75dbda7c9830c24ce5024970e160ff8e24a8c2
Valhugnatt <valentin.philipp@picsellia.com>
2026-07-02 12:01:19 +0200

    remove n samples from param

--- pipelines/dataset_diversified_subset/utils/parameters.py
+++ pipelines/dataset_diversified_subset/utils/parameters.py
@@ -7,11 +7,6 @@ from picsellia_cv_engine.core.parameters import Parameters
 class ProcessingParameters(Parameters):
     def __init__(self, log_data: LogDataType):
         super().__init__(log_data=log_data)
-        self.n_samples = self.extract_parameter(
-            ["n_samples"],
-            expected_type=int,
-            range_value=(1, float("inf")),
-        )
         self.embedder_key = self.extract_parameter(
             ["embedder_key"],
             expected_type=Union[str, None],
